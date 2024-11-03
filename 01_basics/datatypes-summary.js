832247d256b0b07a38d421db0808b3996e28b59e
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2024-11-03 22:05:01 +0000

    summary of data types in JS

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,46 @@
+//# Primitives Data Type
+//7 categories String, Number, Boolean, null,undefined,Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+// console.log(id == anotherId);
+
+
+const bigNumber = 3456236652447852566n 
+
+
+
+
+//Reference Type (Non primitive)
+
+//Array, Objects, Functions 
+
+//array
+const heroes = ["shaktiman","naagraj","doga"];
+
+//object
+let myObj = {
+    name : "Anil",
+    age :22,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+console.log(typeof anotherId)
+
+
